0bdf6c8d3fbe3c2f8e22de2adec18f6e8941aa7e
Ninikareem <Kareemganiyattitilayo@gmail.com>
2022-10-17 16:11:11 -0700

    was not nice

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
-* main - Prints numbers between 0 to 9 and letters between a to f.
+* main - Prints a hexadecimal string
 *
-* Return: Always 0 (Success)
+* Return: Always (Success)
 */
 int main(void)
 {
-	int i;
-	
-	for (i = 48; i < 58; i++)
-{
-	putchar(i);
-}
-for (i = 97; i < 103; i++)
-{
-	putchar(i);
-}
-putchar('\n');
-return (0);
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
