7fd95a6faa21b6f16f53c6bdb490641b57ca5da2
Kareem Ganiyat Titilayo <114157823+Ninikareem@users.noreply.github.com>
2022-10-24 22:48:11 +0100

    first commit in arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - takes the pointer and updates the value
+ *  reset_to_98 - takes the pointer and updates the value
  *  @n: int to check
  *  Return: 0 is success
  */
